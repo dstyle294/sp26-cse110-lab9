3e203a23196122055e57afc4cb7db4c4ba9c448b
dstyle294 <skdalal@ucsd.edu>
2026-05-19 11:36:32 -0700

    feat: ids for buttons + getting buttons in script

---
+++ assets/scripts/main.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const logButton = document.getElementById('log');
+    const errorButton = document.getElementById('error');
+    const tableButton = document.getElementById('table');
+    const dirButton = document.getElementById('dir');
+    const dirxmlButton = document.getElementById('dirxml');
+    const groupButton = document.getElementById('group');
+    const timeButton = document.getElementById('start-time');
+    const timeEndButton = document.getElementById('end-time');
+    const traceButton = document.getElementById('trace');
+})
+
